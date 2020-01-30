d26519f6b461f70190e499d231da204d523925fe
Troels <bentburger21@gmail.com>
2020-01-30 19:36:11 +0100

    Game engine updated

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ var sword = require('./weapon.js');
 var dice = require('./dice.js');
 var persistence = require('./persistence.js');
 
-var p = persistence.load();
+//var p = persistence.load();
+var p = null;
 
 var player = build.player("Troels");
 player.weapon = sword;
@@ -26,17 +27,31 @@ async function arena(player, enemy) {
     2a. kan være sværd damage + terning
     3. slå terning for enemy til at forsvarer sig
     3a. kan være defence + terning
-    4. træk resultaterne fra hinanden og se hvor meget i skade forsvaren får
-    5. Gentag men byt om på hvem der angriber og forsvarer
+    */
+    
+   /*
+    4. Træk resultaterne fra hinanden og 
+        se hvor meget i skade forsvaren får
+    5. Gentag men byt om på hvem der angriber og 
+            forsvarer
     */
 
     while(true) {
         console.log("press any key to start first round1");
         await keypress();
         
+        var angreb = dice.result(20) + player.weapon.damage + player.stats.strength;
+        var defence = dice.result(20) + enemy.defence;
+        console.log(player.name + "'s angreb er " + 
+                    angreb);
+        console.log(enemy.name + "'s forsvar er " + defence);
+        console.log("Resultatet = " + (angreb-defence));
+        var result = angreb-defence;
+        enemy.health -= result;
+        console.log(enemy.health);
+
         if (player.health < 1 || enemy.health < 1)
         { break; }
-        player.health = 0;
     }
 
     process.exit();
